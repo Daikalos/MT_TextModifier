28334c3ae99e53e64976e6683c986e7c0ea10401
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-04-30 13:09:35 +0200

    Project Creation

---
+++ Multithreading_04/Other/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace Multithreading_04
+{
+    static class Extensions
+    {
+        public static void InvokeIfRequired(this Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
---
+++ Multithreading_04/Other/StaticRandom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multithreading_04
+{
+    class StaticRandom
+    {
+        private static readonly Random myRandom = new Random();
+        private static readonly object mySyncLock = new object();
+
+        public static int RandomNumber(int min, int max)
+        {
+            lock (mySyncLock)
+            { // synchronize
+                return myRandom.Next(min, max);
+            }
+        }
+
+        public static double RandomDouble()
+        {
+            lock (mySyncLock)
+            { // synchronize
+                return myRandom.NextDouble();
+            }
+        }
+    }
+}
